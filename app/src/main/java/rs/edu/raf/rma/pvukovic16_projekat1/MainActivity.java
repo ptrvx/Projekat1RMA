4d6ffc0c32ee8777d6aea080039e6ac328512824
Petar Vukovic <petar.vukovic@hotmail.com>
2019-04-19 15:01:36 +0200

    initial commit

---
+++ app/src/main/java/rs/edu/raf/rma/pvukovic16_projekat1/MainActivity.java
@@ -0,0 +1,44 @@
+package rs.edu.raf.rma.pvukovic16_projekat1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import rs.edu.raf.rma.pvukovic16_projekat1.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
